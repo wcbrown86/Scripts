14de3f994ce129ffd164a512d6bd1c641a0ab7c3
William Brown <williambrown@Williams-MacBook-Pro.local>
2018-06-17 02:07:02 -0400

    Started create popup edit function

--- CodeCopy.py
+++ CodeCopy.py
@@ -1,12 +1,32 @@
 from Classes.Terminal_Commands import Terminal
+from Classes.PopupWindows import PopupWindows
 
 
 def on_special_copy():
 
     from_clipboard = Terminal.pull_from_clipboard()
-    from_clipboard = "<code> \n" + from_clipboard + "\n </code>"
-    from_clipboard = '<br />\n'.join(from_clipboard.split("\n"))
-    Terminal.push_to_clipboard(from_clipboard)
+
+    if check_char_count(from_clipboard) is True:
+        from_clipboard = "<code> \n" + from_clipboard + "\n </code>"
+        from_clipboard = '<br />\n'.join(from_clipboard.split("\n"))
+        Terminal.push_to_clipboard(from_clipboard)
+    else:
+        # create_popup_edit(from_clipboard)
+        print('You Have to much text')
+
+
+def check_char_count(input):
+
+    count = len(input)
+
+    if count < 4000:
+        return True
+    else:
+        return False
+
+
+def create_popup_edit(input):
+    popup = PopupWindows()
 
 
 on_special_copy()
