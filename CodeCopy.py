6a37327aeaf716f66f9cb62002ccebf58060f8b4
William Brown <brownwc86@gmail.com>
2018-06-29 01:59:17 -0400

    finished new feature for text splitting

--- CodeCopy.py
+++ CodeCopy.py
@@ -8,21 +8,27 @@
                     formatted and placed baack in the clipboard. The text formatted to
                     keep its formatting when pulled from the terminal prompt and pasted
                     into a text box on a web based application.
+
+                    If the text is to long by the character count, that is defined from the
+                    variable max_char_count. Then the text is split up by the line, then before 
+                    this count is exceeded by the next line four new line character are added 
+                    to give a visual at the point the text needs to be broken up. This new
+                    output is then placed into a text editor that the user can change, as long
+                    as this editor can be called from the command line. Once in the editor 
+                    the user can recopy the text in the right size chunk and rerun the program 
+                    on each chunk to get the right formatting. 
 """
 
 # Imports needed for the program to function. uses prebuilt classes
 # Classes are used so the code can be reused as needed.
 from Classes.Terminal_Commands import Terminal
 
+max_char_count = 4000
 
 # This function uses the Terminal Class to pull the information from the clipboard
 # the string is returned from the function call. and stored in from_clipboard.
 # The the text is edited and a nother function call is made to add the updated
 # Text back to the clip board.
-#
-# Currently working on added a count feature that checks to make sure the string is
-# not too long for the text box. If it is too long the user is shown a text box with the
-# code to allow the user to reduce the length of the text.
 def on_special_copy():
 
     from_clipboard = Terminal.pull_from_clipboard()
@@ -34,22 +40,36 @@ def on_special_copy():
         from_clipboard = '<br />\n'.join(from_clipboard.split("\n"))
         Terminal.push_to_clipboard(from_clipboard)
     else:
-        Terminal.open_VSCode(from_clipboard)
+        split_string = split_chunks(from_clipboard)
+        Terminal.open_VSCode(split_string)
 
 
 # The function checks the length of the proved string, if the string is too long then
 # the function returns false, otherwise it returns true.
-#
-# Current output is incorrect for the purpose of testing.
 def check_char_count(input):
 
     count = len(input)
 
-    if count < 4000:
+    if count < max_char_count:
         return True
     else:
         return False
 
+
+def split_chunks(contents):
+    next_split = max_char_count
+    string_array = contents.split('\n')
+    new_string = ''
+
+    for word in string_array:
+        if len(word) + len(new_string) > next_split:
+            new_string += '\n\n\n\n'
+            new_string += word
+            next_split += max_char_count
+        else:
+            new_string += '\n' + word
+
+    return new_string
 
 # Calls the on_special_copy function to start the program.
 on_special_copy()
